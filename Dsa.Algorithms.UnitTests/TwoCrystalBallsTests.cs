671604079b1b2bb7c6c9450aeb566547cfd9096e
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-10 06:37:45 +0800

    feat: Added two crystal balls algorithm

---
+++ Dsa.Algorithms.UnitTests/TwoCrystalBallsTests.cs
@@ -0,0 +1,20 @@
+namespace Dsa.Algorithms.UnitTests
+{
+    using Dsa.Algorithms;
+    using FluentAssertions;
+
+    public sealed class TwoCrystalBallsTests
+    {
+        [Theory]
+        [InlineData(new[] { false, false, false, false, false, false, false, false, false, false, false, false, false, false }, -1)]
+        [InlineData(new[] { false, false, false, false, false, false, false, true, true, true, true, true, true, true }, 7)]
+        [InlineData(new[] { false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true }, 9)]
+        [InlineData(new[] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true }, 0)]
+        public void FindIndex_ShouldFindTheFirstIndex(bool[] breaks, int expectedIndex)
+        {
+            var actualIndex = TwoCrystalBalls.FindIndex(breaks);
+
+            actualIndex.Should().Be(expectedIndex);
+        }
+    }
+}
---
+++ Dsa.Algorithms/TwoCrystalBalls.cs
@@ -0,0 +1,45 @@
+﻿namespace Dsa.Algorithms
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Given an array of booleans, try to find the index of the first occurrence of desired item
+    /// optimally. The array given is sorted.
+    /// </summary>
+    public static class TwoCrystalBalls
+    {
+        /// <summary>
+        /// Searches for the first index of the occurrence by leaping instead of linearly from
+        /// start to found. Time complexity: O(sqrt(n)).
+        /// </summary>
+        /// <param name="breaks">The array of sorted booleans.</param>
+        /// <returns>The found index of the first occurrence. <code>-1</code> if not found.</returns>
+        public static int FindIndex(IEnumerable<bool> breaks)
+        {
+            var jumpAmount = Convert.ToInt32(Math.Floor(Math.Sqrt(breaks.Count())));
+
+            var index = jumpAmount;
+
+            for (; index < breaks.Count(); index += jumpAmount)
+            {
+                if (breaks.ElementAt(new Index(index)))
+                {
+                    break;
+                }
+            }
+
+            index -= jumpAmount;
+
+            for (var jndex = 0; jndex < jumpAmount && index < breaks.Count(); ++index, ++jndex)
+            {
+                if (breaks.ElementAt(new Index(index)))
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
