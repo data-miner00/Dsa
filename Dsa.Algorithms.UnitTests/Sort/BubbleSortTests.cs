473168459673404f0cfc2cde62843160502c3c97
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-10 13:51:29 +0800

    feat: Bubble sort

---
+++ Dsa.Algorithms.UnitTests/Sort/BubbleSortTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Dsa.Algorithms.UnitTests.Sort
+{
+    using Dsa.Algorithms.Sort;
+
+    public sealed class BubbleSortTests
+    {
+        [Fact]
+        public void BubbleSort_SortArrayInPlaceCorrectly()
+        {
+            var array = new[] { 5, 2, 6, 3, 6, 7, 2, 1, 6, 483, 43, 12, 9 };
+            var expectedArray = new[] { 1, 2, 2, 3, 5, 6, 6, 6, 7, 9, 12, 43, 483 };
+
+            array.Length.Should().Be(expectedArray.Length);
+
+            BubbleSort.Sort(array);
+
+            array.Should().BeEquivalentTo(expectedArray);
+        }
+    }
+}
---
+++ Dsa.Algorithms/Sort/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿namespace Dsa.Algorithms.Sort
+{
+    /// <summary>
+    /// The bubble sort implementation. Time complexity: O(n^2).
+    /// </summary>
+    public static class BubbleSort
+    {
+        public static void Sort<T>(T[] list)
+            where T : IComparable<T>
+        {
+            for (var i = 0; i < list.Length; i++)
+            {
+                for (var j = 0; j < list.Length - i - 1; j++)
+                {
+                    if (list[j].CompareTo(list[j + 1]) > 0)
+                    {
+                        (list[j + 1], list[j]) = (list[j], list[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
