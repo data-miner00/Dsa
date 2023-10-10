743af2bb0a963349cfe545dd2b5902ae65e405c0
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-10 13:16:17 +0800

    feat: Binary search

---
+++ Dsa.Algorithms.UnitTests/BinarySearchTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Dsa.Algorithms.UnitTests
+{
+    using Dsa.Algorithms;
+    using FluentAssertions;
+
+    public sealed class BinarySearchTests
+    {
+        [Fact]
+        public void BinarySearch_ShouldFoundCorrespondingItem()
+        {
+            var ints = new[] { 1, 2, 3, 999, 53, 478, 120, 330, 742, 1000, 3092, 32 };
+
+            Array.Sort(ints);
+
+            ints.IsContains(458).Should().BeFalse();
+            ints.IsContains(1).Should().BeTrue();
+            ints.IsContains(32).Should().BeTrue();
+            ints.IsContains(1000).Should().BeTrue();
+            ints.IsContains(478).Should().BeTrue();
+        }
+    }
+}
---
+++ Dsa.Algorithms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿namespace Dsa.Algorithms
+{
+    /// <summary>
+    /// Binary search implementation with two pointers. Time complexity: O(n).
+    /// The list must be sorted in a non-decreasing order.
+    /// </summary>
+    public static class BinarySearch
+    {
+        public static bool IsContains<T>(this T[] haystack, T needle)
+            where T : IComparable<T>
+        {
+            var lo = 0;
+            var hi = haystack.Length;
+
+            do
+            {
+                var midpoint = Convert.ToInt32(Math.Floor((double)lo + (hi - lo) / 2));
+                var currentElem = haystack[midpoint];
+                
+                if (currentElem.Equals(needle))
+                {
+                    return true;
+                }
+                else if (currentElem.CompareTo(needle) > 0) // CurrentElement > needle
+                {
+                    hi = midpoint;
+                }
+                else if (currentElem.CompareTo(needle) < 0) // CurrentElement < needle
+                {
+                    lo = midpoint + 1;
+                }
+            } while (lo < hi);
+
+            return false;
+        }
+    }
+}
