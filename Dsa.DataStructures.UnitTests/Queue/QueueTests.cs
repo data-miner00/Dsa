f01c41c32ebf19a13ab0226d002d27717d47314b
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-10 08:02:51 +0800

    feat: Added queue

---
+++ Dsa.DataStructures.UnitTests/Queue/QueueTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Dsa.DataStructures.UnitTests.Queue
+{
+    using Dsa.DataStructures.Queue;
+    using FluentAssertions;
+
+    public sealed class QueueTests
+    {
+        [Fact]
+        public void Enqueue_ShouldAddItemsCorrectly()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            queue.Peek().Should().Be(1);
+            queue.Length.Should().Be(3);
+            queue.Tail.Value.Should().Be(3);
+        }
+
+        [Fact]
+        public void Dequeue_ShouldRemoveItemCorrectly()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            queue.Dequeue().Should().Be(1);
+            queue.Peek().Should().Be(2);
+            queue.Tail.Value.Should().Be(4);
+            queue.Length.Should().Be(3);
+        }
+    }
+}
---
+++ Dsa.DataStructures/Queue/Queue.cs
@@ -0,0 +1,73 @@
+﻿namespace Dsa.DataStructures.Queue
+{
+    public sealed class Node<T>
+    {
+        public static Node<T> Create(T value) => new() { Value = value };
+
+        public T Value { get; set; }
+
+        public Node<T>? Next { get; set; }
+    }
+
+    /// <summary>
+    /// The queue data structure.
+    /// </summary>
+    /// <typeparam name="T">The type of data.</typeparam>
+    public sealed class Queue<T>
+    {
+        public int Length { get; private set; } = 0;
+
+        public Node<T>? Head { get; private set; }
+
+        public Node<T>? Tail { get; private set; }
+
+        public void Enqueue(T value)
+        {
+            var node = Node<T>.Create(value);
+
+            this.Length++;
+
+            if (this.Head == null || this.Tail == null)
+            {
+                this.Head = this.Tail = node;
+                return;
+            }
+
+            this.Tail.Next = node;
+            this.Tail = node;
+        }
+
+        public T? Dequeue()
+        {
+            if (this.Head == null)
+            {
+                return default;
+            }
+
+            this.Length--;
+
+            var head = this.Head;
+
+            this.Head = head.Next;
+
+            head.Next = null;
+
+            if (this.Length == 0)
+            {
+                this.Tail = null;
+            }
+
+            return head.Value;
+        }
+
+        public T? Peek()
+        {
+            if (this.Head == null)
+            {
+                return default;
+            }
+
+            return this.Head.Value;
+        }
+    }
+}
