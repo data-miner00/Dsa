bbd849623f341a65e9481f6b5ff860247209491f
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-11 18:50:22 +0800

    feat: Implement array list

---
+++ Dsa.DataStructures.UnitTests/ArrayList/ArrayListTests.cs
@@ -0,0 +1,107 @@
+﻿namespace Dsa.DataStructures.UnitTests.ArrayList
+{
+    using Dsa.DataStructures.ArrayList;
+
+    public sealed class ArrayListTests
+    {
+        [Fact]
+        public void Prepend_ItemShouldInsertedInFront()
+        {
+            var list = new ArrayList<int>();
+
+            list.Prepend(1);
+            list.Capacity.Should().Be(2);
+
+            list.Prepend(2);
+            list.Prepend(3);
+
+            list.Get(0).Should().Be(3);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(1);
+            list.Length.Should().Be(3);
+            list.Capacity.Should().Be(4);
+        }
+
+        [Fact]
+        public void Append_ItemShouldInsertedAtBack()
+        {
+            var list = new ArrayList<int>();
+            
+            list.Append(1);
+            list.Append(2);
+            list.Capacity.Should().Be(2);
+
+            list.Append(3);
+
+            list.Get(0).Should().Be(1);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(3);
+            list.Length.Should().Be(3);
+            list.Capacity.Should().Be(4);
+        }
+
+        [Fact]
+        public void InsertAt_ItemShouldInsertAtCorrectPosition()
+        {
+            var list = new ArrayList<int>();
+
+            list.Append(1);
+            list.Prepend(5);
+            list.Append(23);
+            list.InsertAt(2, 999);
+
+            list.Get(2).Should().Be(999);
+        }
+
+        [Fact]
+        public void InsertAt_ExpansionShouldDoneCorrectly()
+        {
+            var list = new ArrayList<int>();
+
+            list.Append(1);
+            list.Capacity.Should().Be(2);
+            list.InsertAt(1, 54);
+
+            list.Get(1).Should().Be(54);
+            list.Length.Should().Be(2);
+            list.Capacity.Should().Be(4);
+        }
+
+        [Fact]
+        public void Remove_FoundItem_ItemRemoved()
+        {
+            var list = new ArrayList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+            list.Append(5);
+            list.Length.Should().Be(5);
+
+            list.Remove(4);
+            list.Length.Should().Be(4);
+
+            list.Get(3).Should().Be(5);
+        }
+
+        [Fact]
+        public void RemoveAt_FoundItem_ItemRemoved()
+        {
+            var list = new ArrayList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+            list.Append(5);
+            list.Length.Should().Be(5);
+
+            list.RemoveAt(2);
+            list.RemoveAt(2);
+
+            list.Length.Should().Be(3);
+            list.Get(2).Should().Be(5);
+        }
+    }
+}
---
+++ Dsa.DataStructures/ArrayList/ArrayList.cs
@@ -0,0 +1,156 @@
+﻿namespace Dsa.DataStructures.ArrayList
+{
+    public sealed class ArrayList<T>
+    {
+        // Get: O(1) Push/Pop: O(1) Unshift: O(n)
+        public int Capacity { get; set; } = 2;
+
+        public T[] Values { get; private set; }
+
+        public int Length { get; private set; } = 0;
+
+        public ArrayList()
+        {
+            this.Values = new T[this.Capacity];
+        }
+
+        public void Prepend(T item)
+        {
+            if (this.Length == this.Capacity - 1)
+            {
+                this.Capacity *= 2;
+                var original = (T[])this.Values.Clone();
+                this.Values = new T[this.Capacity];
+
+                this.Values[0] = item;
+                for (var i = 0; i < original.Length; i++)
+                {
+                    this.Values[i + 1] = original[i];
+                }
+
+                this.Length++;
+
+                return;
+            }
+
+            for (var i = this.Length; i > 0; i--)
+            {
+                this.Values[i] = this.Values[i - 1];
+            }
+
+            this.Values[0] = item;
+
+            this.Length++;
+        }
+
+        public void InsertAt(int index, T item)
+        {
+            if (this.Length == this.Capacity - 1)
+            {
+                this.Capacity *= 2;
+                var original = this.Values;
+                this.Values = new T[this.Capacity];
+
+                var i = 0;
+                for (; i < index; i++)
+                {
+                    this.Values[i] = original[i];
+                }
+
+                this.Values[index] = item; // index
+                
+                for (; i < original.Length; i++)
+                {
+                    this.Values[i + 1] = original[i];
+                }
+
+                this.Length++;
+
+                return;
+            }
+
+            for (var i = this.Length; i > index; i--)
+            {
+                this.Values[i] = this.Values[i - 1];
+            }
+
+            this.Values[index] = item;
+
+            this.Length++;
+        }
+
+        public void Append(T item)
+        {
+            if (this.Length == this.Capacity)
+            {
+                this.Capacity *= 2;
+                var original = this.Values;
+                this.Values = new T[this.Capacity];
+
+                var i = 0;
+                for (; i < original.Length; i++)
+                {
+                    this.Values[i] = original[i];
+                }
+
+                this.Values[i] = item;
+
+                this.Length++;
+                return;
+            }
+
+            this.Values[this.Length] = item;
+            this.Length++;
+        }
+
+        public T? Remove(T item)
+        {
+            var isFoundItem = false;
+            var i = 0;
+            for (; i < this.Length; i++)
+            {
+                isFoundItem = this.Values[i].Equals(item);
+
+                if (isFoundItem)
+                {
+                    break;
+                }
+            }
+
+            if (isFoundItem)
+            {
+                for (; i < this.Length; i++)
+                {
+                    this.Values[i - 1] = this.Values[i];
+                }
+                this.Length--;
+                return item;
+            }
+
+            return default;
+        }
+
+        public T? Get(int index)
+        {
+            ref var value = ref this.Values[index];
+            return value;
+        }
+
+        public T? RemoveAt(int index)
+        {
+            var removeValue = this.Values[index];
+
+            for (var i = index; i < this.Length; i++)
+            {
+                this.Values[i] = this.Values[i + 1];
+            }
+
+            if (removeValue != null)
+            {
+                this.Length--;
+            }
+
+            return removeValue;
+        }
+    }
+}
