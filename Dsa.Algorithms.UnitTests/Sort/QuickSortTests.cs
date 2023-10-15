c2690bd102380420bd1cc84f087b2a43dd8fae51
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-15 14:26:29 +0800

    feat: Add quick sort

---
+++ Dsa.Algorithms.UnitTests/Sort/QuickSortTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Dsa.Algorithms.UnitTests.Sort
+{
+    using Dsa.Algorithms.Sort;
+
+    public sealed class QuickSortTests
+    {
+        [Fact]
+        public void QuickSort_ShouldSortTheArrayCorrectly()
+        {
+            var numbers = new[] { 6, 2, 7, 56, 3, 7, 9 };
+            var expected = new[] { 2, 3, 6, 7, 7, 9, 56 };
+
+            QuickSort.Sort(numbers);
+
+            numbers.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ Dsa.Algorithms/Sort/QuickSort.cs
@@ -0,0 +1,50 @@
+﻿namespace Dsa.Algorithms.Sort
+{
+    /// <summary>
+    /// The implementation of QuickSort.
+    /// </summary>
+    public static class QuickSort
+    {
+        private static void Qs(int[] arr, int lo, int hi)
+        {
+            if (lo >= hi)
+            {
+                return;
+            }
+
+            var pivotIdx = Partition(arr, lo, hi);
+
+            Qs(arr, lo, pivotIdx - 1);
+            Qs(arr, pivotIdx + 1, hi);
+        }
+
+        private static int Partition(int[] arr, int lo, int hi)
+        {
+            var pivot = arr[hi];
+            var idx = lo - 1;
+
+            for (var i = lo; i < hi; ++i)
+            {
+                if (arr[i] <= pivot)
+                {
+                    idx++;
+                    var tmp = arr[i];
+                    arr[i] = arr[idx];
+                    arr[idx] = tmp;
+                }
+            }
+
+            idx++;
+
+            arr[hi] = arr[idx];
+            arr[idx] = pivot;
+
+            return idx;
+        }
+        
+        public static void Sort(int[] numbers)
+        {
+            Qs(numbers, 0, numbers.Length - 1);
+        }
+    }
+}
