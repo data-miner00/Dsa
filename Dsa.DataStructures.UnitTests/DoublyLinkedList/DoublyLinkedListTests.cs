4668e30900b3452103c7ca4b842bae302ca6c954
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-15 17:56:01 +0800

    feat: Add doubly linked list

---
+++ Dsa.DataStructures.UnitTests/DoublyLinkedList/DoublyLinkedListTests.cs
@@ -0,0 +1,127 @@
+﻿namespace Dsa.DataStructures.UnitTests.DoublyLinkedList
+{
+    using Dsa.DataStructures.DoublyLinkedList;
+
+    public sealed class DoublyLinkedListTests
+    {
+        [Fact]
+        public void Prepend_ItemsInsertedAtCorrectPosition()
+        {
+            var list = new DoublyLinkedList<int>();
+
+            list.Prepend(1);
+            list.Prepend(2);
+            list.Prepend(3);
+
+            list.Length.Should().Be(3);
+
+            list.Get(0).Should().Be(3);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(1);
+
+            list.Head.Value.Should().Be(3);
+            list.Tail.Value.Should().Be(1);
+        }
+
+        [Fact]
+        public void Append_ItemsInsertedAtCorrectPosition()
+        {
+            var list = new DoublyLinkedList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+
+            list.Length.Should().Be(3);
+
+            list.Get(0).Should().Be(1);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(3);
+
+            list.Head.Value.Should().Be(1);
+            list.Tail.Value.Should().Be(3);
+        }
+
+        [Fact]
+        public void InsertAt_ItemsInsertedAtCorrectPosition()
+        {
+            var list = new DoublyLinkedList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+            list.Append(5);
+
+            list.Length.Should().Be(5);
+
+            list.InsertAt(3, 100);
+
+            list.Length.Should().Be(6);
+
+            list.Get(0).Should().Be(1);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(3);
+            list.Get(3).Should().Be(100);
+            list.Get(4).Should().Be(4);
+            list.Get(5).Should().Be(5);
+
+            list.Head.Value.Should().Be(1);
+            list.Tail.Value.Should().Be(5);
+        }
+
+        [Fact]
+        public void Remove_ItemsFoundRemovedCorrectly()
+        {
+            var list = new DoublyLinkedList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+            list.Append(5);
+
+            list.Length.Should().Be(5);
+
+            var expected = list.Remove(3);
+            expected.Should().Be(3);
+
+            list.Length.Should().Be(4);
+
+            list.Get(0).Should().Be(1);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(4);
+            list.Get(3).Should().Be(5);
+
+            list.Head.Value.Should().Be(1);
+            list.Tail.Value.Should().Be(5);
+        }
+
+        [Fact]
+        public void RemoveAt_ValidIndexItemRemoved()
+        {
+            var list = new DoublyLinkedList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+            list.Append(5);
+
+            list.Length.Should().Be(5);
+
+            var expected = list.RemoveAt(2);
+            expected.Should().Be(3);
+
+            list.Length.Should().Be(4);
+
+            list.Get(0).Should().Be(1);
+            list.Get(1).Should().Be(2);
+            list.Get(2).Should().Be(4);
+            list.Get(3).Should().Be(5);
+
+            list.Head.Value.Should().Be(1);
+            list.Tail.Value.Should().Be(5);
+        }
+    }
+}
---
+++ Dsa.DataStructures/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,186 @@
+﻿namespace Dsa.DataStructures.DoublyLinkedList
+{
+    public sealed class Node<T>
+    {
+        public static Node<T> Create(T item) => new() { Value = item };
+
+        public T? Value { get; set; }
+
+        public Node<T>? Next { get; set; }
+
+        public Node<T>? Prev { get; set; }
+    }
+    
+    /// <summary>
+    /// Doubly linked list. Time complexity: O(n).
+    /// </summary>
+    /// <typeparam name="T">The homogeneous type to be stored.</typeparam>
+    public sealed class DoublyLinkedList<T>
+    {
+        public int Length { get; private set; } = 0;
+
+        public Node<T>? Head { get; private set; }
+
+        public Node<T>? Tail { get; private set; }
+
+        public void Prepend(T item)
+        {
+            var node = Node<T>.Create(item);
+
+            this.Length++;
+
+            if (this.Head == null && this.Tail == null)
+            {
+                this.Head = this.Tail = node;
+                return;
+            }
+
+            node.Next = this.Head;
+            this.Head.Prev = node;
+            this.Head = node;
+        }
+
+        public void InsertAt(int index, T item)
+        {
+            if (index < 0 || index > this.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (index == this.Length)
+            {
+                this.Append(item);
+                return;
+            }
+            else if (index == 0)
+            {
+                this.Prepend(item);
+                return;
+            }
+
+            var node = Node<T>.Create(item);
+            this.Length++;
+
+            var currentNode = this.Head;
+
+            for (var i = 1; currentNode != null && i < index; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            var nextNode = currentNode.Next;
+            currentNode.Next = node;
+            node.Next = nextNode;
+            nextNode.Prev = node;
+            node.Prev = currentNode;
+        }
+
+        public void Append(T item)
+        {
+            this.Length++;
+
+            var node = Node<T>.Create(item);
+
+            if (this.Tail == null && this.Head == null)
+            {
+                this.Tail = this.Head = node;
+                return;
+            }
+
+            node.Prev = this.Tail;
+            this.Tail.Next = node;
+            this.Tail = node;
+        }
+
+        public T? Remove(T item)
+        {
+            var currentNode = this.Head;
+
+            for (var i = 0; currentNode != null && i < this.Length; i++)
+            {
+                if (currentNode.Value.Equals(item))
+                {
+                    break;
+                }
+                currentNode = currentNode.Next;
+            }
+
+            // Fail to delete anything, return.
+            if (currentNode == null)
+            {
+                return default;
+            }
+            
+            return this.RemoveNode(currentNode);
+        }
+
+        public T? Get(int index)
+        {
+            var node = this.GetAt(index);
+            if (node == null)
+            {
+                return default;
+            }
+
+            return node.Value;
+        }
+
+        public T? RemoveAt(int index)
+        {
+            var currentNode = this.GetAt(index);
+
+            if (currentNode == null)
+            {
+                return default;
+            }
+
+            return this.RemoveNode(currentNode);
+        }
+
+        private T? RemoveNode(Node<T> node)
+        {
+            this.Length--;
+
+            if (this.Length == 0)
+            {
+                var value = this.Head.Value;
+                this.Head = this.Tail = null;
+                return value;
+            }
+
+            if (node.Prev != null)
+            {
+                node.Prev.Next = node.Next;
+            }
+
+            if (node.Next != null)
+            {
+                node.Next.Prev = node.Prev;
+            }
+
+            if (node == this.Head)
+            {
+                this.Head = node.Next;
+            }
+
+            if (node == this.Tail)
+            {
+                this.Tail = node.Prev;
+            }
+
+            node.Prev = node.Next = null;
+            return node.Value;
+        }
+
+        private Node<T>? GetAt(int index)
+        {
+            var currentNode = this.Head;
+
+            for (var i = 0; currentNode != null && i < index; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            return currentNode;
+        }
+    }
+}
