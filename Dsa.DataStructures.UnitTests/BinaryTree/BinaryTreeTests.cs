520e15f5943c6ea18d950b0db618491cfb40c5a0
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-17 19:01:59 +0800

    feat: Added dfs binary tree

---
+++ Dsa.DataStructures.UnitTests/BinaryTree/BinaryTreeTests.cs
@@ -0,0 +1,129 @@
+﻿namespace Dsa.DataStructures.UnitTests.BinaryTree
+{
+    using Dsa.DataStructures.BinaryTree;
+
+    public sealed class BinaryTreeTests
+    {
+        [Fact]
+        public void PreOrderTraversal_ShouldTraverseInCorrectOrder()
+        {
+            var expected = new[] { 20, 10, 5, 7, 15, 50, 30, 29, 45, 100 };
+        
+            var actual = BinaryTree.PreOrderTraversal(GetTree());
+
+            actual.ToArray().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void InOrderTraversal_ShouldTraverseInCorrectOrder()
+        {
+            var expected = new[] { 5, 7, 10, 15, 20, 29, 30, 45, 50, 100 };
+
+            var actual = BinaryTree.InOrderTraversal(GetTree());
+
+            actual.ToArray().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void PostOrderTraversal_ShouldTraverseInCorrectOrder()
+        {
+            var expected = new[] { 7, 5, 15, 10, 29, 45, 30, 100, 50, 20 };
+        
+            var actual = BinaryTree.PostOrderTraversal(GetTree());
+
+            actual.ToArray().Should().BeEquivalentTo(expected);
+        }
+
+        public static BinaryNode<int> GetTree()
+        {
+            return new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new()
+                {
+                    Value = 10,
+                    Left = new()
+                    {
+                        Value = 5,
+                        Right = new()
+                        {
+                            Value = 7,
+                        },
+                    },
+                    Right = new()
+                    {
+                        Value = 15,
+                    },
+                },
+                Right = new()
+                {
+                    Value = 50,
+                    Left = new()
+                    {
+                        Value = 30,
+                        Left = new()
+                        {
+                            Value = 29,
+                        },
+                        Right = new()
+                        {
+                            Value = 45,
+                        },
+                    },
+                    Right = new()
+                    {
+                        Value = 100,
+                    },
+                },
+            };
+        }
+
+        public static BinaryNode<int> GetTree2()
+        {
+            return new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new()
+                {
+                    Value = 10,
+                    Left = new()
+                    {
+                        Value = 5,
+                        Right = new()
+                        {
+                            Value = 7,
+                        },
+                    },
+                    Right = new()
+                    {
+                        Value = 15,
+                    },
+                },
+                Right = new()
+                {
+                    Value = 50,
+                    Left = new()
+                    {
+                        Value = 30,
+                        Left = new()
+                        {
+                            Value = 29,
+                            Left = new()
+                            {
+                                Value = 21,
+                            },
+                        },
+                        Right = new()
+                        {
+                            Value = 45,
+                            Right = new()
+                            {
+                                Value = 49,
+                            },
+                        },
+                    },
+                },
+            };
+        }
+    }
+}
---
+++ Dsa.DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,87 @@
+﻿namespace Dsa.DataStructures.BinaryTree
+{
+    public sealed class BinaryNode<T>
+    {
+        public T? Value { get; set; }
+
+        public BinaryNode<T>? Left { get; set; }
+
+        public BinaryNode<T>? Right { get;set; }
+    }
+
+    /// <summary>
+    /// Depth-first search on a binary tree.
+    /// </summary>
+    public static class BinaryTree
+    {
+        private static Queue<T> WalkPreOrder<T>(BinaryNode<T>? current, Queue<T> path)
+        {
+            if (current == null)
+            {
+                return path;
+            }
+
+            // Pre
+            path.Enqueue(current.Value);
+
+            // Recurese
+            WalkPreOrder(current.Left, path);
+            WalkPreOrder(current.Right, path);
+
+            // Post
+            return path;
+        }
+
+        private static Queue<T> WalkInOrder<T>(BinaryNode<T>? current, Queue<T> path)
+        {
+            if (current == null)
+            {
+                return path;
+            }
+
+            // Pre
+
+            // Recurese
+            WalkInOrder(current.Left, path);
+            path.Enqueue(current.Value);
+            WalkInOrder(current.Right, path);
+
+            // Post
+            return path;
+        }
+
+        private static Queue<T> WalkPostOrder<T>(BinaryNode<T>? current, Queue<T> path)
+        {
+            if (current == null)
+            {
+                return path;
+            }
+
+            // Pre
+
+            // Recurese
+            WalkPostOrder(current.Left, path);
+            WalkPostOrder(current.Right, path);
+
+            // Post
+            path.Enqueue(current.Value);
+            return path;
+        }
+
+        public static IEnumerable<T> PreOrderTraversal<T>(BinaryNode<T> head)
+        {
+            return WalkPreOrder(head, new Queue<T>()).AsEnumerable();
+        }
+
+        public static IEnumerable<T> InOrderTraversal<T>(BinaryNode<T> head)
+        {
+
+            return WalkInOrder(head, new Queue<T>()).AsEnumerable();
+        }
+
+        public static IEnumerable<T> PostOrderTraversal<T>(BinaryNode<T> head)
+        {
+            return WalkPostOrder(head, new Queue<T>()).AsEnumerable();
+        }
+    }
+}
