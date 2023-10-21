061f83dba12e2cb515c57e3bef65c3a6d1a67841
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-21 18:24:48 +0800

    test: Use fixture

---
+++ Dsa.DataStructures.UnitTests/BinaryTree/BinaryTreeFixture.cs
@@ -0,0 +1,122 @@
+﻿namespace Dsa.DataStructures.UnitTests.BinaryTree
+{
+    using System;
+    using Dsa.DataStructures.BinaryTree;
+
+    public sealed class BinaryTreeFixture : IDisposable
+    {
+        private bool isDisposed = false;
+
+        public BinaryTreeFixture()
+        {
+            this.Tree1 = GetTree();
+            this.Tree2 = GetTree2();
+        }
+
+        public BinaryNode<int> Tree1 { get; private set; }
+
+        public BinaryNode<int> Tree2 { get; private set; }
+
+        public void Dispose()
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+
+            this.isDisposed = true;
+        }
+
+        private static BinaryNode<int> GetTree()
+        {
+            return new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new()
+                {
+                    Value = 10,
+                    Left = new()
+                    {
+                        Value = 5,
+                        Right = new()
+                        {
+                            Value = 7,
+                        },
+                    },
+                    Right = new()
+                    {
+                        Value = 15,
+                    },
+                },
+                Right = new()
+                {
+                    Value = 50,
+                    Left = new()
+                    {
+                        Value = 30,
+                        Left = new()
+                        {
+                            Value = 29,
+                        },
+                        Right = new()
+                        {
+                            Value = 45,
+                        },
+                    },
+                    Right = new()
+                    {
+                        Value = 100,
+                    },
+                },
+            };
+        }
+
+        private static BinaryNode<int> GetTree2()
+        {
+            return new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new()
+                {
+                    Value = 10,
+                    Left = new()
+                    {
+                        Value = 5,
+                        Right = new()
+                        {
+                            Value = 7,
+                        },
+                    },
+                    Right = new()
+                    {
+                        Value = 15,
+                    },
+                },
+                Right = new()
+                {
+                    Value = 50,
+                    Left = new()
+                    {
+                        Value = 30,
+                        Left = new()
+                        {
+                            Value = 29,
+                            Left = new()
+                            {
+                                Value = 21,
+                            },
+                        },
+                        Right = new()
+                        {
+                            Value = 45,
+                            Right = new()
+                            {
+                                Value = 49,
+                            },
+                        },
+                    },
+                },
+            };
+        }
+    }
+}
--- Dsa.DataStructures.UnitTests/BinaryTree/BinaryTreeTests.cs
+++ Dsa.DataStructures.UnitTests/BinaryTree/BinaryTreeTests.cs
@@ -1,15 +1,25 @@
 ﻿namespace Dsa.DataStructures.UnitTests.BinaryTree
 {
     using Dsa.DataStructures.BinaryTree;
+    using Xunit.Abstractions;
 
-    public sealed class BinaryTreeTests
+    public sealed class BinaryTreeTests : IClassFixture<BinaryTreeFixture>
     {
+        private readonly BinaryTreeFixture fixture;
+        private readonly ITestOutputHelper output;
+
+        public BinaryTreeTests(BinaryTreeFixture fixture, ITestOutputHelper output)
+        {
+            this.fixture = fixture;
+            this.output = output;
+        }
+
         [Fact]
         public void PreOrderTraversal_ShouldTraverseInCorrectOrder()
         {
             var expected = new[] { 20, 10, 5, 7, 15, 50, 30, 29, 45, 100 };
-        
-            var actual = BinaryTree.PreOrderTraversal(GetTree());
+
+            var actual = BinaryTree.PreOrderTraversal(this.fixture.Tree1);
 
             actual.ToArray().Should().BeEquivalentTo(expected);
         }
@@ -19,7 +29,7 @@
         {
             var expected = new[] { 5, 7, 10, 15, 20, 29, 30, 45, 50, 100 };
 
-            var actual = BinaryTree.InOrderTraversal(GetTree());
+            var actual = BinaryTree.InOrderTraversal(this.fixture.Tree1);
 
             actual.ToArray().Should().BeEquivalentTo(expected);
         }
@@ -28,102 +38,10 @@
         public void PostOrderTraversal_ShouldTraverseInCorrectOrder()
         {
             var expected = new[] { 7, 5, 15, 10, 29, 45, 30, 100, 50, 20 };
-        
-            var actual = BinaryTree.PostOrderTraversal(GetTree());
+
+            var actual = BinaryTree.PostOrderTraversal(this.fixture.Tree1);
 
             actual.ToArray().Should().BeEquivalentTo(expected);
-        }
-
-        public static BinaryNode<int> GetTree()
-        {
-            return new BinaryNode<int>
-            {
-                Value = 20,
-                Left = new()
-                {
-                    Value = 10,
-                    Left = new()
-                    {
-                        Value = 5,
-                        Right = new()
-                        {
-                            Value = 7,
-                        },
-                    },
-                    Right = new()
-                    {
-                        Value = 15,
-                    },
-                },
-                Right = new()
-                {
-                    Value = 50,
-                    Left = new()
-                    {
-                        Value = 30,
-                        Left = new()
-                        {
-                            Value = 29,
-                        },
-                        Right = new()
-                        {
-                            Value = 45,
-                        },
-                    },
-                    Right = new()
-                    {
-                        Value = 100,
-                    },
-                },
-            };
-        }
-
-        public static BinaryNode<int> GetTree2()
-        {
-            return new BinaryNode<int>
-            {
-                Value = 20,
-                Left = new()
-                {
-                    Value = 10,
-                    Left = new()
-                    {
-                        Value = 5,
-                        Right = new()
-                        {
-                            Value = 7,
-                        },
-                    },
-                    Right = new()
-                    {
-                        Value = 15,
-                    },
-                },
-                Right = new()
-                {
-                    Value = 50,
-                    Left = new()
-                    {
-                        Value = 30,
-                        Left = new()
-                        {
-                            Value = 29,
-                            Left = new()
-                            {
-                                Value = 21,
-                            },
-                        },
-                        Right = new()
-                        {
-                            Value = 45,
-                            Right = new()
-                            {
-                                Value = 49,
-                            },
-                        },
-                    },
-                },
-            };
         }
     }
 }
