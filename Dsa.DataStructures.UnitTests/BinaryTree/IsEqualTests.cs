f90b1f128d6baf7b885383ded874768c4405177d
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-26 17:52:57 +0800

    feat: Added binary tree comparison

---
+++ Dsa.DataStructures.UnitTests/BinaryTree/IsEqualTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Dsa.DataStructures.UnitTests.BinaryTree
+{
+    using Dsa.DataStructures.BinaryTree;
+
+    public sealed class IsEqualTests : IClassFixture<BinaryTreeFixture>
+    {
+        private readonly BinaryTreeFixture fixture;
+
+        public IsEqualTests(BinaryTreeFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void IsEqual_EqualTrees_ShouldReturnTrue()
+        {
+            var actual = this.fixture.Tree1.IsEqual(this.fixture.Tree1);
+            actual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsEqual_DifferentTrees_ShouldReturnFalse()
+        {
+            var actual = this.fixture.Tree1.IsEqual(this.fixture.Tree2);
+            actual.Should().BeFalse();
+        }
+    }
+}
---
+++ Dsa.DataStructures/BinaryTree/IsEqual.cs
@@ -0,0 +1,25 @@
+﻿namespace Dsa.DataStructures.BinaryTree
+{
+    public static partial class BinaryTree
+    {
+        public static bool IsEqual<T>(this BinaryNode<T>? self, BinaryNode<T>? other)
+        {
+            if (self == null && other == null)
+            {
+                return true;
+            }
+
+            if (self == null || other == null)
+            {
+                return false;
+            }
+
+            if (!self.Value.Equals(other.Value))
+            {
+                return false;
+            }
+
+            return IsEqual(self.Left, other.Left) && IsEqual(self.Right, other.Right);
+        }
+    }
+}
