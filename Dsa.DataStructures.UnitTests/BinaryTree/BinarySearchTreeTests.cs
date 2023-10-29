68ec95f6ffdb785facfd4b198267777021c4b9ab
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-29 17:31:10 +0800

    feat: Add binary search tree find and insert

---
+++ Dsa.DataStructures.UnitTests/BinaryTree/BinarySearchTreeTests.cs
@@ -0,0 +1,127 @@
+﻿namespace Dsa.DataStructures.UnitTests.BinaryTree
+{
+    using Dsa.DataStructures.BinaryTree;
+
+    public sealed class BinarySearchTreeTests : IClassFixture<BinaryTreeFixture>
+    {
+        private readonly BinaryTreeFixture fixture;
+
+        public BinarySearchTreeTests(BinaryTreeFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Theory]
+        [InlineData(45)]
+        [InlineData(7)]
+        public void Find_DataExist_ReturnTrue(int value)
+        {
+            var actual = BinarySearchTree.Find(this.fixture.Tree1, value);
+            actual.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(49)]
+        [InlineData(123)]
+        [InlineData(4)]
+        public void Find_DataNotExist_ReturnFalse(int value)
+        {
+            var actual = BinarySearchTree.Find(this.fixture.Tree1, value);
+            actual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Insert_LessThanNode_InsertedLeft()
+        {
+            /*
+             *   (20)                  (20)
+             *   /  \     insert 35    /  \
+             * (5)  (56)  -------->  (5)  (56)
+             *                            /
+             *                          (35)
+             */
+            var tree = new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new BinaryNode<int>
+                {
+                    Value = 5,
+                },
+                Right = new BinaryNode<int>
+                {
+                    Value = 56,
+                },
+            };
+
+            var expectedTree = new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new BinaryNode<int>
+                {
+                    Value = 5,
+                },
+                Right = new BinaryNode<int>
+                {
+                    Value = 56,
+                    Left = new BinaryNode<int>
+                    {
+                        Value = 35,
+                    },
+                },
+            };
+
+            BinarySearchTree.Insert(tree, 35);
+
+            BinaryTree.IsEqual(tree, expectedTree)
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void Insert_MoreThanNode_InsertedRight()
+        {
+            /*
+             *   (20)                   (20)
+             *   /  \     insert 100    /  \
+             * (5)  (56)  --------->  (5)  (56)
+             *                               \
+             *                              (100)
+             */
+            var tree = new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new BinaryNode<int>
+                {
+                    Value = 5,
+                },
+                Right = new BinaryNode<int>
+                {
+                    Value = 56,
+                },
+            };
+
+            var expectedTree = new BinaryNode<int>
+            {
+                Value = 20,
+                Left = new BinaryNode<int>
+                {
+                    Value = 5,
+                },
+                Right = new BinaryNode<int>
+                {
+                    Value = 56,
+                    Right = new BinaryNode<int>
+                    {
+                        Value = 100,
+                    },
+                },
+            };
+
+            BinarySearchTree.Insert(tree, 100);
+
+            BinaryTree.IsEqual(tree, expectedTree)
+                .Should()
+                .BeTrue();
+        }
+    }
+}
---
+++ Dsa.DataStructures/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,93 @@
+﻿namespace Dsa.DataStructures.BinaryTree
+{
+    using System;
+
+    /// <summary>
+    /// Collection of methods for Binary Search Tree (BST).
+    /// </summary>
+    public static class BinarySearchTree
+    {
+        /// <summary>
+        /// Find an element in a Binary Search Tree. Complexity: between O(log n) to O(n)
+        /// </summary>
+        /// <typeparam name="T">Type that is comparable.</typeparam>
+        /// <param name="node">The head of the BST.</param>
+        /// <param name="value">The value to find.</param>
+        /// <returns>The flag that indicates whether the element was found.</returns>
+        public static bool Find<T>(BinaryNode<T>? node, T value)
+            where T : IComparable<T>
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (node.Value.Equals(value))
+            {
+                return true;
+            }
+
+            if (node.Value.CompareTo(value) < 0)
+            {
+                return Find(node.Right, value);
+            }
+
+            return Find(node.Left, value);
+        }
+
+        /// <summary>
+        /// Inserts an element into a Binary Search Tree.
+        /// </summary>
+        /// <typeparam name="T">Type that is comparable.</typeparam>
+        /// <param name="node">The head of BST.</param>
+        /// <param name="value">The value to insert.</param>
+        public static void Insert<T>(BinaryNode<T> node, T value)
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(node.Value) <= 0 && node.Left != null)
+            {
+                Insert(node.Left, value);
+                return;
+            }
+
+            if (value.CompareTo(node.Value) <= 0 && node.Left == null)
+            {
+                node.Left = new BinaryNode<T> { Value = value };
+                return;
+            }
+
+            if (value.CompareTo(node.Value) > 0 && node.Right != null)
+            {
+                Insert(node.Right, value);
+                return;
+            }
+
+            node.Right = new BinaryNode<T> { Value = value };
+        }
+
+        // TODO: Implement deletion
+        public static void Delete<T>(BinaryNode<T>? node, T value)
+            where T : IComparable<T>
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.Value.Equals(value) && node.Right != null)
+            {
+                
+                
+            }
+            else if (node.Left != null && node.Left.Left == null && node.Left.Right == null && node.Left.Value.Equals(value))
+            {
+                node.Left = null;
+            }
+            else if (node.Right != null && node.Right.Left == null && node.Right.Right == null && node.Right.Value.Equals(value))
+            {
+                node.Right = null;
+            }
+            
+        }
+    }
+}
