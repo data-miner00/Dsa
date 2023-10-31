aeed4aa71b8d70d0a3557d5aa4b51e00b602f4fe
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-31 20:38:31 +0800

    test: Added test priority orderer

---
+++ Dsa.DataStructures.UnitTests/TestPriorityAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Dsa.DataStructures.UnitTests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : Attribute
+    {
+        public TestPriorityAttribute(int priority)
+        {
+            this.Priority = priority;
+        }
+
+        public int Priority { get; private set; }
+    }
+}
