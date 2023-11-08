122ced75dc2bfa4c36e259c49cc46ea662c54295
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-08 21:55:55 +0800

    feat: Added Dijkstra

---
+++ Dsa.Algorithms.UnitTests/Greedy/DijkstraAlgorithmTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Dsa.Algorithms.UnitTests.Greedy
+{
+    using Dsa.Algorithms.Greedy;
+    using Dsa.DataStructures.Graph.AdjacencyList;
+
+    public sealed class DijkstraAlgorithmTests
+    {
+        private readonly GraphEdge[][] graph;
+
+        public DijkstraAlgorithmTests()
+        {
+            /*      (1) --- (4) ---- (5)
+            //    /  |       |       /|
+            // (0)   | ------|------- |
+            //    \  |/      |        |
+            //      (2) --- (3) ---- (6) */
+            this.graph = new[]
+            {
+                new[]
+                {
+                    new GraphEdge(1, 3),
+                    new GraphEdge(2, 1),
+                },
+                new[]
+                {
+                    new GraphEdge(0, 3),
+                    new GraphEdge(2, 4),
+                    new GraphEdge(4, 1),
+                },
+                new[]
+                {
+                    new GraphEdge(1, 4),
+                    new GraphEdge(3, 7),
+                    new GraphEdge(0, 1),
+                },
+                new[]
+                {
+                    new GraphEdge(2, 7),
+                    new GraphEdge(4, 5),
+                    new GraphEdge(6, 1),
+                },
+                new[]
+                {
+                    new GraphEdge(1, 1),
+                    new GraphEdge(3, 5),
+                    new GraphEdge(5, 2),
+                },
+                new[]
+                {
+                    new GraphEdge(6, 1),
+                    new GraphEdge(4, 2),
+                    new GraphEdge(2, 18),
+                },
+                new[]
+                {
+                    new GraphEdge(3, 1),
+                    new GraphEdge(5, 2),
+                },
+            };
+        }
+
+        [Fact]
+        public void ShortestPath_ShouldReturnShortestPath()
+        {
+            var actual = DijkstraAlgorithm.ShortestPath(this.graph, 0, 6);
+
+            actual.Should().BeEquivalentTo(new[] { 0, 1, 4, 5, 6 });
+        }
+    }
+}
---
+++ Dsa.Algorithms/Greedy/DijkstraAlgorithm.cs
@@ -0,0 +1,89 @@
+﻿namespace Dsa.Algorithms.Greedy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Dsa.DataStructures.Graph.AdjacencyList;
+
+    public static class DijkstraAlgorithm
+    {
+        public static int[] ShortestPath(GraphEdge[][] graph, int source, int sink)
+        {
+            var seen = new bool[graph.Length];
+            Array.Fill(seen, false);
+
+            var prev = new int[graph.Length];
+            Array.Fill(prev, -1);
+
+            var dists = new int[graph.Length];
+            Array.Fill(dists, int.MaxValue);
+            dists[source] = 0;
+
+            while (HasUnvisited(seen, dists))
+            {
+                var current = GetLowestUnvisited(seen, dists);
+
+                seen[current] = true;
+
+                var adjs = graph[current];
+                for (var i = 0; i < adjs.Length; ++i)
+                {
+                    var edge = adjs[i];
+                    if (seen[edge.To])
+                    {
+                        continue;
+                    }
+
+                    var dist = dists[current] + edge.Weight;
+                    if (dist < dists[edge.To])
+                    {
+                        dists[edge.To] = dist;
+                        prev[edge.To] = current;
+                    }
+                }
+            }
+
+            var output = new Queue<int>();
+            var curr = sink;
+
+            while (prev[curr] != -1)
+            {
+                output.Enqueue(curr);
+                curr = prev[curr];
+            }
+
+            output.Enqueue(source);
+
+            return output.Reverse().ToArray();
+        }
+
+        private static bool HasUnvisited(bool[] seen, int[] dists)
+        {
+            var zipped = seen.Zip(dists);
+
+            return zipped.Any((zip) => !zip.First && zip.Second < int.MaxValue);
+        }
+
+        private static int GetLowestUnvisited(bool[] seen, int[] dists)
+        {
+            var index = -1;
+            var lowestDistance = int.MaxValue;
+
+            for (var i = 0; i < seen.Length; i++)
+            {
+                if (seen[i])
+                {
+                    continue;
+                }
+
+                if (lowestDistance > dists[i])
+                {
+                    lowestDistance = dists[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
