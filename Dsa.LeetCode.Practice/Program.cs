48cd7b0420ccdee78f91a7c703db19c95bc887a1
data-miner00 <data-miner00@users.noreply.github.com>
2024-01-30 08:31:46 +0800

    feat: Add leetcode practice

---
+++ Dsa.LeetCode.Practice/Program.cs
@@ -0,0 +1,8 @@
+﻿using Dsa.LeetCode.Practice;
+
+Console.WriteLine(Zigzag.Convert("PAYPALISHIRING", 3));
+Console.WriteLine(Zigzag.Convert("PAYPALISHIRING", 4));
+Console.WriteLine(Zigzag.Convert("PAYPALISHIRING", 5));
+Console.WriteLine(Zigzag.Convert("A", 1));
+Console.WriteLine(Zigzag.Convert("ABCD", 3));
+Console.WriteLine(Zigzag.Convert("ABCDE", 4));
---
+++ Dsa.LeetCode.Practice/Zigzag.cs
@@ -0,0 +1,59 @@
+﻿namespace Dsa.LeetCode.Practice
+{
+    using System.Text;
+
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/zigzag-conversion/description/">Zigzag Conversion</see>.
+    /// </summary>
+    public static class Zigzag
+    {
+        /// <summary>
+        /// Convert to a linearly read, zigzag string.
+        /// <see href="https://leetcode.com/problems/zigzag-conversion/submissions/1160546719/">My submission link</see>.
+        /// </summary>
+        /// <param name="s">The string to be converted.</param>
+        /// <param name="rows">The number of rows the zigzag pattern requires.</param>
+        /// <returns>The converted string.</returns>
+        public static string Convert(string s, int rows)
+        {
+            var sb = new StringBuilder();
+            var len = s.Length;
+            var gap = (rows * 2) - 2;
+            if (gap == 0)
+            {
+                gap++;
+            }
+
+            var bars = 1 + ((len - rows) / gap);
+            var rem = (len - rows) % gap;
+
+            if (rem > 0)
+            {
+                bars++;
+            }
+
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < bars; j++)
+                {
+                    if ((i == 0 || i == rows - 1) && (gap * j) + i < len)
+                    {
+                        sb.Append(s[(gap * j) + i]);
+                    }
+
+                    if (j > 0 && i > 0 && i < rows - 1 && (gap * j) - i < len)
+                    {
+                        sb.Append(s[(gap * j) - i]);
+                    }
+
+                    if (i > 0 && i < rows - 1 && (gap * j) + i < len)
+                    {
+                        sb.Append(s[(gap * j) + i]);
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
