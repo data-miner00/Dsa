5c2da0530008e49756321366dd464b2214b44512
data-miner00 <data-miner00@users.noreply.github.com>
2024-05-07 19:01:46 +0800

    feat: Add diagonal traversal

---
+++ Dsa.Algorithms.UnitTests/DiagonalTraversalTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Dsa.Algorithms.UnitTests;
+
+using System.Collections.Generic;
+
+public sealed class DiagonalTraversalTests
+{
+    [Fact]
+    public void Traverse_SimpleMatrix_ExpectedResults()
+    {
+        List<List<int>> matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        List<int> expected = [1, 2, 4, 7, 5, 3, 6, 8, 9];
+
+        var result = DiagonalTraversal.Traverse(matrix);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Dsa.Algorithms/DiagonalTraversal.cs
@@ -0,0 +1,59 @@
+﻿namespace Dsa.Algorithms;
+
+using System.Collections.Generic;
+
+public static class DiagonalTraversal
+{
+    public static List<int> Traverse(List<List<int>> matrix)
+    {
+        var result = new List<int>();
+        var goingUp = true;
+        int i = 0, j = 0;
+
+        for (var k = 0; k < matrix[0].Count * matrix.Count;)
+        {
+            if (goingUp)
+            {
+                for (; i >= 0 && j < matrix[0].Count; i--, j++)
+                {
+                    result.Add(matrix[i][j]);
+                    k++;
+                }
+
+                if (i <= 0)
+                {
+                    i++;
+                }
+
+                if (j >= matrix[0].Count)
+                {
+                    i++;
+                    j--;
+                }
+            }
+            else
+            {
+                for (; i < matrix.Count && j >= 0; j--, i++)
+                {
+                    result.Add(matrix[i][j]);
+                    k++;
+                }
+
+                if (j <= 0)
+                {
+                    j++;
+                }
+
+                if (i >= matrix.Count)
+                {
+                    i--;
+                    j++;
+                }
+            }
+
+            goingUp = !goingUp;
+        }
+
+        return result;
+    }
+}
