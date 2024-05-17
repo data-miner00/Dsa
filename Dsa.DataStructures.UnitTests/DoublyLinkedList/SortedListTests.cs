c0960e91252d7878f2f36c75c32eae9c399483ee
data-miner00 <data-miner00@users.noreply.github.com>
2024-05-17 20:51:14 +0800

    feat: Self made sorted list

---
+++ Dsa.DataStructures.UnitTests/DoublyLinkedList/SortedListTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Dsa.DataStructures.UnitTests.DoublyLinkedList;
+
+using Dsa.DataStructures.DoublyLinkedList;
+
+public sealed class SortedListTests
+{
+    [Fact]
+    public void Insert_WithoutOrder_OrderedOutput()
+    {
+        var sortedList = new SortedList<int>();
+
+        sortedList.Insert(5);
+        sortedList.Insert(1);
+        sortedList.Insert(3);
+        sortedList.Insert(4);
+        sortedList.Insert(2);
+        sortedList.Insert(6);
+
+        var ascending = (int[])Array.CreateInstance(typeof(int), sortedList.Length);
+        var descending = (int[])Array.CreateInstance(typeof(int), sortedList.Length);
+        Node<int> front = sortedList.Head;
+        Node<int> back = sortedList.Tail;
+
+        for (int i = 0; i < sortedList.Length; i++)
+        {
+            ascending[i] = front.Value;
+            descending[i] = back.Value;
+            front = front.Next;
+            back = back.Prev;
+        }
+
+        int[] expectedAscending = [1, 2, 3, 4, 5, 6];
+        int[] expectedDescending = [6, 5, 4, 3, 2, 1];
+
+        Assert.Equal(expectedAscending, ascending);
+        Assert.Equal(expectedDescending, descending);
+    }
+}
---
+++ Dsa.DataStructures/DoublyLinkedList/SortedList.cs
@@ -0,0 +1,70 @@
+﻿namespace Dsa.DataStructures.DoublyLinkedList;
+
+using System;
+
+public sealed class SortedList<T>
+    where T : IComparable<T>
+{
+    public int Length { get; private set; } = 0;
+
+    public Node<T>? Head { get; private set; }
+
+    public Node<T>? Tail { get; private set; }
+
+    public void Insert(T item)
+    {
+        var node = new Node<T>
+        {
+            Value = item,
+        };
+
+        if (this.Head is null)
+        {
+            this.Head = this.Tail = node;
+        }
+        else if (this.Head is not null && this.Head == this.Tail)
+        {
+            if (item.CompareTo(this.Head.Value) > 0)
+            {
+                this.Tail = node;
+                this.Head.Next = this.Tail;
+                this.Tail.Prev = this.Head;
+            }
+            else
+            {
+                this.Head = node;
+                this.Tail.Prev = this.Head;
+                this.Head.Next = this.Tail;
+            }
+        }
+        else
+        {
+            var currentNode = this.Head;
+
+            for (var i = 0; currentNode != null; i++)
+            {
+                if (item.CompareTo(currentNode.Value) < 0)
+                {
+                    var prev = currentNode.Prev;
+                    prev.Next = node;
+                    node.Prev = prev;
+                    node.Next = currentNode;
+                    currentNode.Prev = node;
+                    break;
+                }
+
+                currentNode = currentNode.Next;
+            }
+
+            if (currentNode is null)
+            {
+                var last = this.Tail;
+                last.Next = node;
+                node.Prev = last;
+                this.Tail = node;
+            }
+        }
+
+        this.Length++;
+    }
+}
