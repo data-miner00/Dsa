ec3419684cee0aeb3441c4185360a087fac0503e
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-10 07:47:49 +0800

    feat: Add factorial implementations

---
+++ Dsa.Recursion.UnitTests/RecursionTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Dsa.Recursion.UnitTests
+{
+    using Dsa.Recursion;
+
+    public sealed class RecursionTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        [InlineData(10, 3_628_800)]
+        public void Normal_GivenNumber_ExpectCorrectFactorial(int number, int expected)
+        {
+            var actual = Factorial.Normal(number);
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        [InlineData(10, 3_628_800)]
+        public void TailRecursive_GivenNumber_ExpectCorrectFactorial(int number, int expected)
+        {
+            var actual = Factorial.TailRecursive(number);
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        [InlineData(10, 3_628_800)]
+        public void Recursive_GivenNumber_ExpectCorrectFactorial(int number, int expected)
+        {
+            var actual = Factorial.Recursive(number);
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ Dsa.Recursion/Factorial.cs
@@ -0,0 +1,64 @@
+﻿namespace Dsa.Recursion
+{
+    /// <summary>
+    /// Different implementations of factorial!.
+    /// </summary>
+    public static class Factorial
+    {
+        /// <summary>
+        /// For loop implementation of factorial.
+        /// </summary>
+        /// <param name="number">The number to calculate.</param>
+        /// <returns>The factorial.</returns>
+        public static int Normal(int number)
+        {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Tail recursive implementation of factorial.
+        /// </summary>
+        /// <param name="number">The number to calculate.</param>
+        /// <returns>The factorial.</returns>
+        public static int TailRecursive(int number)
+        {
+            return Helper(number, 1);
+
+            static int Helper(int number, int accumulator)
+            {
+                if (number == 1)
+                {
+                    return accumulator;
+                }
+
+                return Helper(number - 1, accumulator * number);
+            }
+        }
+
+        /// <summary>
+        /// Recursive implementation of factorial.
+        /// </summary>
+        /// <param name="number">The number to calculate.</param>
+        /// <returns>The factorial.</returns>
+        public static int Recursive(int number)
+        {
+            return Helper(number);
+
+            static int Helper(int current)
+            {
+                if (current == 1)
+                {
+                    return current;
+                }
+
+                return Helper(current - 1) * current;
+            }
+        }
+    }
+}
