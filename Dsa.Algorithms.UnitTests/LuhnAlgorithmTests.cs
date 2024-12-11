5f6f1cbb98e3f6a6bbea77fe85b747489199734a
Shaun Chong <data-miner00@users.noreply.github.com>
2024-12-11 22:54:03 +0800

    feat: Add Luhn Algorithm

---
+++ Dsa.Algorithms.UnitTests/LuhnAlgorithmTests.cs
@@ -0,0 +1,75 @@
+﻿namespace Dsa.Algorithms.UnitTests
+{
+    using System.Collections.Generic;
+
+    public sealed class LuhnAlgorithmTests
+    {
+        public static IEnumerable<object[]> ValidCardNumberData()
+        {
+            yield return new object[] { "374245455400126" };
+            yield return new object[] { "5425233430109903" };
+            yield return new object[] { "4701322211111234" };
+        }
+
+        public static IEnumerable<object[]> InvalidCardNumberData()
+        {
+            yield return new object[] { "574245455400126" };
+            yield return new object[] { "6425233430109903" };
+            yield return new object[] { "4701322281111234" };
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidCardNumberData))]
+        public void Validate_ValidCardNumber_ReturnTrue(string cardNumber)
+        {
+            var actual = LuhnAlgorithm.Validate(cardNumber);
+
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCardNumberData))]
+        public void Validate_InvalidCardNumber_ReturnFalse(string cardNumber)
+        {
+            var actual = LuhnAlgorithm.Validate(cardNumber);
+
+            Assert.False(actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidCardNumberData))]
+        public void ValidateV2_ValidCardNumber_ReturnTrue(string cardNumber)
+        {
+            var actual = LuhnAlgorithm.ValidateV2(cardNumber);
+
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCardNumberData))]
+        public void ValidateV2_InvalidCardNumber_ReturnFalse(string cardNumber)
+        {
+            var actual = LuhnAlgorithm.ValidateV2(cardNumber);
+
+            Assert.False(actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidCardNumberData))]
+        public void ValidateV3_ValidCardNumber_ReturnTrue(string cardNumber)
+        {
+            var actual = LuhnAlgorithm.ValidateV3(cardNumber);
+
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCardNumberData))]
+        public void ValidateV3_InvalidCardNumber_ReturnFalse(string cardNumber)
+        {
+            var actual = LuhnAlgorithm.ValidateV3(cardNumber);
+
+            Assert.False(actual);
+        }
+    }
+}
---
+++ Dsa.Algorithms/LuhnAlgorithm.cs
@@ -0,0 +1,96 @@
+﻿namespace Dsa.Algorithms
+{
+    public static class LuhnAlgorithm
+    {
+        /// <summary>
+        /// The Luhn's algorithm modified from <see href="https://en.wikipedia.org/wiki/Luhn_algorithm">Wikipedia</see>.
+        /// </summary>
+        /// <param name="cardNumber">The card number.</param>
+        /// <returns>Whether the number is valid or not.</returns>
+        public static bool Validate(string cardNumber)
+        {
+            int sum = 0;
+            int parity = cardNumber.Length % 2;
+
+            for (int i = 0; i < cardNumber.Length; i++)
+            {
+                int digit = cardNumber[i] - '0';
+
+                if (i % 2 != parity)
+                {
+                    sum += digit;
+                }
+                else if (digit > 4)
+                {
+                    sum += (2 * digit) - 9;
+                }
+                else
+                {
+                    sum += 2 * digit;
+                }
+            }
+
+            return sum % 10 == 0;
+        }
+
+        /// <summary>
+        /// The Luhn's algorithm implementation from <see href="https://www.geeksforgeeks.org/luhn-algorithm/">GeeksForGeeks</see>.
+        /// </summary>
+        /// <param name="cardNumber">The card number.</param>
+        /// <returns>Whether the number is valid or not.</returns>
+        public static bool ValidateV2(string cardNumber)
+        {
+            int sum = 0;
+            bool isSecond = false;
+
+            for (int i = cardNumber.Length - 1; i >= 0; --i)
+            {
+                int digit = cardNumber[i] - '0';
+
+                if (isSecond)
+                {
+                    digit *= 2;
+                }
+
+                sum += digit / 10;
+                sum += digit % 10;
+
+                isSecond = !isSecond;
+            }
+
+            return sum % 10 == 0;
+        }
+
+        /// <summary>
+        /// The Luhn's algorithm implemented from <see href="https://en.wikipedia.org/wiki/Luhn_algorithm">Wikipedia</see>.
+        /// </summary>
+        /// <param name="cardNumber">The card number.</param>
+        /// <returns>Whether the number is valid or not.</returns>
+        public static bool ValidateV3(string cardNumber)
+        {
+            int sum = 0;
+            int parity = cardNumber.Length % 2;
+            int checkDigit = cardNumber[^1] - '0';
+
+            for (int i = 0; i < cardNumber.Length - 1; i++)
+            {
+                int digit = cardNumber[i] - '0';
+
+                if (i % 2 != parity)
+                {
+                    sum += digit;
+                }
+                else if (digit > 4)
+                {
+                    sum += (2 * digit) - 9;
+                }
+                else
+                {
+                    sum += 2 * digit;
+                }
+            }
+
+            return checkDigit == (10 - (sum % 10)) % 10;
+        }
+    }
+}
